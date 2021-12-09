5f7a243ba26eeff11da28a359f3ccdc9dd7b15e6
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-12-09 22:43:47 +0300

    Update 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,8 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
+-- sql script to create a trigger that decreses the quantity
+-- after adding new order
 
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders 
+FOR EACH ROW 
+UPDATE items SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name
