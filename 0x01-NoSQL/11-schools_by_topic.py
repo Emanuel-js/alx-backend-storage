59f8c7843b6cf47d380fcfc3964821167be0f724
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-12-17 22:32:15 +0300

    Create 11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+doc doc doc
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    doc doc doc
+    """
+    return mongo_collection.find({"topics": topic})
